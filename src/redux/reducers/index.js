5b4b9dead123fe0b53f37a68c612310563d99c80
Mohit Kapoor <kapoormohit01@gmail.com>
2020-05-05 22:52:26 +0530

    removing an extra reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import inputs from './todos'
-import visibilityFilter from './visibilityFilter'
 
 export default combineReducers({
-  inputs,
-  visibilityFilter
+  inputs
 })
